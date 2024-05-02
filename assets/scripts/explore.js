56a7831ed4129d2831b733e0981b96c6fe2371d1
yuvasaro <yuvanand.s@gmail.com>
2024-05-02 11:12:48 -0700

    Explore - Speech Synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,40 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let faceImage = document.querySelector("[alt='Smiling face']");
+  let textArea = document.getElementById("text-to-speak");
+  let voiceSelect = document.getElementById("voice-select");
+  let button = document.getElementsByTagName("button")[0];
+  const synth = window.speechSynthesis;
+  let voices;
+
+  setTimeout(() => {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      let option = document.createElement("option");
+      option.text = voices[i].name;
+      voiceSelect.appendChild(option);
+    }
+  }, 10);
+
+  button.addEventListener('click', function() {
+    if (synth.speaking) {
+      synth.cancel();
+    }
+    let utterance = new SpeechSynthesisUtterance(textArea.value);
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name == voiceSelect.value) {
+        utterance.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utterance);
+    faceImage.src = "assets/images/smiling-open.png";
+    let checkSpeaking = setInterval(() => {
+      if (!synth.speaking) {
+        faceImage.src = "assets/images/smiling.png";
+        clearInterval(checkSpeaking);
+      }
+    });
+  });
 }
